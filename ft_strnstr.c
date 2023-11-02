c4d2201e2b9596d79cd39a51202800953e2ca9eb
Alexandr Pimikov <apimikov@c3r4p4.hive.fi>
2023-11-02 09:58:03 +0200

    strnstr modified

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apimikov <apimikov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 06:50:31 by apimikov          #+#    #+#             */
+/*   Updated: 2023/11/02 09:02:55 by apimikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	len_n;
 
 	i = 0;
 
-	while (*haystack)
-	{
-		if (*haystack = *needle)
-		{
+	len_n = len + 1;
 
-#
-		}
-	}
-	while (i < n)
-	{
-		if (s1[i] != s2[i] || s1[i] == '\0' || s2[i] == '\0')
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-	return (0);
+	len_n = ft_strlen(needle);
+	if (!len_n)
+		return ((char *)haystack);
+	return ((char *)needle);
+//	while (*haystack && i + len_n <= len)
+//	{
+//		if (*haystack == *needle && ft_strncmp(haystack, needle, len_n) == 0)
+//			return ((char *)haystack);
+//		haystack++;
+//	}
+
+//	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -7,7 +7,7 @@
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 
-size_t  ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *b, size_t len);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
