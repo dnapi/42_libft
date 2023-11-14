1b0946d106b0f4db9283c1446645256fd71ee571
Alexandr Pimikov <pimikov@gmail.com>
2023-11-14 11:22:34 +0200

    norms

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: apimikov <apimikov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 13:13:34 by apimikov          #+#    #+#             */
-/*   Updated: 2023/11/14 10:17:25 by apimikov         ###   ########.fr       */
+/*   Updated: 2023/11/14 11:22:12 by apimikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@ int	inv_sign(int sign)
 int	ft_atoi(const char *str)
 {
 	int			sign;
-	long long 	answ;
-	long long	llong_max_10; 
+	long long	answ;
+	long long	llong_max_10;
 
 	sign = 1;
 	answ = 0;
-	llong_max_10 = ((unsigned long long)(-1))/2/10;
-
+	llong_max_10 = ((unsigned long long)(-1)) / 2 / 10;
 	while ((8 < *str && *str < 14) || *str == ' ')
 		str++;
 	if (*str == '-' || *str == '+')
@@ -36,10 +35,10 @@ int	ft_atoi(const char *str)
 			sign = -1;
 	while (*str != '\0' && '0' <= *str && *str <= '9')
 	{
-	if (answ < llong_max_10 || (answ == llong_max_10 && *str < '8'))
-			 answ = answ * 10 + ((*str++) - '0');
-	else
-		return (inv_sign(sign));
+		if (answ < llong_max_10 || (answ == llong_max_10 && *str < '8'))
+			answ = answ * 10 + ((*str++) - '0');
+		else
+			return (inv_sign(sign));
 	}
 	return ((int)(sign * answ));
 }
